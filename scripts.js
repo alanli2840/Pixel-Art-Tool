f48ee03fd8a70d07161444d568f739740abdeef2
alan li <alanli1ny@gmail.com>
2023-02-19 14:53:51 -0500

    Added input management from slider and styles for tooltips

--- scripts.js
+++ scripts.js
@@ -1,2 +1,9 @@
-let sizeSlider = document.querySelector(".size-slider");
-let sliderText = document.querySelector(".slider-text");
+let sizeSlider = document.querySelector(".size-slider>input");
+let sliderText = document.querySelector(".slider-text>h2");
+
+sliderText.textContent = `${sizeSlider.value} x ${sizeSlider.value}`;
+
+sizeSlider.addEventListener('input', function() {
+    console.log(this.value);
+    sliderText.textContent = `${this.value} x ${this.value}`;
+});
