fcbb6e6c9dcfcdb6faa4d68bc5029750f5eac1e5
alan li <alanli1ny@gmail.com>
2023-03-04 16:37:00 -0500

    Implemented loading and updating of grid

--- scripts.js
+++ scripts.js
@@ -1,9 +1,52 @@
 let sizeSlider = document.querySelector(".size-slider>input");
-let sliderText = document.querySelector(".slider-text>h2");
+let sliderText = document.querySelector(".slider-text>div");
 
-sliderText.textContent = `${sizeSlider.value} x ${sizeSlider.value}`;
+let grid = document.querySelector(".grid");
+let gridBoxes = new Map();
+let gridSize = sizeSlider.value;
+
+const loadGrid = () => {
+    sliderText.textContent = `${gridSize} x ${gridSize}`;
+    for(let i = 1; i <= gridSize * gridSize; i++) {
+        const box = document.createElement("div");
+        grid.appendChild(box).className = `grid-box box${i}`;
+        gridBoxes.set(i, 'none');
+    }
+}
+
+const addBoxes = (gridSizeOld) => {
+    for(let i = gridSizeOld * gridSizeOld + 1; i <= gridSize * gridSize; i++) {
+        const box = document.createElement("div");
+        grid.appendChild(box).className = `grid-box box${i}`;
+        gridBoxes.set(i, 'none');
+    }
+}
+
+const removeBoxes = (gridSizeOld) => {
+    for(let i = gridSize * gridSize + 1; i <= gridSizeOld * gridSizeOld; i++) {
+        const box = document.querySelector(`.box${i}`);
+        box.remove();
+        gridBoxes.delete(i);
+    }
+}
+
+const updateGrid = (gridSizeOld, gridSizeNew) => {
+    gridSize = gridSizeNew;
+    sliderText.textContent = `${gridSize} x ${gridSize}`;
+
+    grid.style.setProperty('--grid-columns', gridSize);
+    grid.style.setProperty('--grid-rows', gridSize);
+
+    if(gridSizeOld < gridSize)
+        addBoxes(gridSizeOld);
+    else
+        removeBoxes(gridSizeOld);
+}
 
 sizeSlider.addEventListener('input', function() {
-    console.log(this.value);
-    sliderText.textContent = `${this.value} x ${this.value}`;
+    if(this.value != gridSize) {
+        updateGrid(gridSize, this.value);
+    }
 });
+
+document.addEventListener('DOMContentLoaded', loadGrid());
