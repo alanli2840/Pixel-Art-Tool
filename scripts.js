339fff01cec948cb36fc3a65b31faac45f8491da
alan li <alanli1ny@gmail.com>
2023-03-07 23:00:38 -0500

    Implemented history management for undo/redo + undo/redo for transforms

--- scripts.js
+++ scripts.js
@@ -19,6 +19,7 @@ let darken = document.querySelector(".darken");
 let mirrorDraw = document.querySelector(".mirror-draw");
 let fill = document.querySelector(".fill");
 let clear = document.querySelector(".clear-button");
+let isCleared = true;
 
 let mouseDown = false;
 document.body.onmousedown = () => mouseDown = true;
@@ -33,14 +34,137 @@ let redoButton = document.querySelector(".redo");
 
 let rotationDeg = 0;
 
-const getBoxElemenet = boxNum => box = document.querySelector(`.box${boxNum}`);
+const getBoxElement = boxNum => box = document.querySelector(`.box${boxNum}`);
+const getBoxColor = boxNum => gridBoxes.get(boxNum);
+
+class changeData {
+    constructor(prevColor, newColor, transformation = null) {
+        this.transformation = transformation;
+        this.prevColor = prevColor
+        this.newColor = newColor;
+    }
+
+    set setTransform(transformation) {
+        this.transformation = transformation;
+    }
+
+    set setPrev(color) {
+        this.prevColor = color;
+    }
+
+    set setNew(color) {
+        this.newColor = color;
+    }
+
+    get getTransform() {
+        if(this.transformation == null) return;
+        return this.prevColor;
+    }
+
+    get getPrev() {
+        if(this.prevColor == null) return;
+        return this.prevColor;
+    }
+
+    get getNew() {
+        if(this.newColor == null) return;
+        return this.newColor;
+    }
+}
+
+class gridStatus {
+    constructor() {
+        this.gridStatus = new Map();
+        this.prevGridStatus = null;
+        this.nextGridStatus = null;
+    }
+
+    get getGridStatus() {
+        return this.gridStatus;
+    }
+
+    get getPrev() {
+        return this.prevGridStatus;
+    }
+
+    get getNext() {
+        return this.nextGridStatus;
+    }
+
+    set setPrev(gridStatus) {
+        this.prevGridStatus = gridStatus;
+    }
+
+    set setNext(gridStatus) {
+        this.nextGridStatus = gridStatus;
+    }
+
+    addStatus(changeData, boxNum = "transform") {
+        this.gridStatus.set(boxNum, changeData);
+    }
+}
+
+class gridHistory {
+    constructor() {
+        this.maxLength = 20;
+        this.index = 0;
+        this.current = new gridStatus();
+        this.head = this.current;
+        this.tail = new gridStatus();
+        this.head.nextGridStatus = this.tail;
+        this.tail.prevGridStatus = this.head;
+    }
+
+    get getCurrent() {
+        return this.current;
+    }
+    
+    set setCurrent(gridStatus) {
+        this.current = gridStatus;
+    }
+
+    updateHistory(gridStatus) {
+        if(this.index == this.maxLength) {
+            this.head = this.head.nextGridStatus;
+            this.head.prevGridStatus = null;
+            this.index--;
+        }
+        this.current.nextGridStatus = gridStatus;
+        gridStatus.prevGridStatus = this.current;
+        this.current = gridStatus;
+        this.current.nextGridStatus = this.tail;
+        this.tail.prevGridStatus = this.current;
+        this.index++;
+    }
+
+    undo() {
+        if(this.current == this.head) return null;
+        let returnVal = this.current;
+        this.current = this.current.prevGridStatus;
+        this.index--;
+        return returnVal;
+    }
+
+    redo() {
+        if(this.current == this.head)
+            this.current = this.head.nextGridStatus;
+        else if(this.current == this.tail) 
+            return null;
+        let returnVal = this.current;
+        this.current = this.current.nextGridStatus;
+        this.index++;
+        return returnVal;
+    }
+}
+
+let history = new gridHistory();
 
 colorPicker.addEventListener('input', function() {
     currentColor = `${this.value}`;
 });
 
 const selectColor = boxNum => {
-    currentColor = gridBoxes.get(boxNum);
+    currentColor = getBoxColor(boxNum);
     colorPicker.value = currentColor;
 };
 
@@ -64,7 +188,7 @@ const checkColorVal = rgbVal => {
 
 //use bitwise operators to change shade of color using it's hex value
 const changeShade = (box, boxNum, shade) => {
-    let currentVal = parseInt(gridBoxes.get(boxNum).substring(1), 16);
+    let currentVal = parseInt(getBoxColor(boxNum).substring(1), 16);
 
     const r = checkColorVal((currentVal >> 16) + shade);
     const g = checkColorVal((currentVal >> 8 & 0x0000ff) + shade);
@@ -111,12 +235,17 @@ const calcOppVer = boxNum => {
 
 const mirrorDrawing = (box, boxNum) => {
   color(box, boxNum, currentColor);
-  const opp = calcOppHor(boxNum);
+  let opp;
+  //mirror draw is always drawing mirrored along the horizontal
+  if((Math.abs(rotationDeg)/90) % 2 == 0)
+    opp = calcOppHor(boxNum);
+  else
+    opp = calcOppVer(boxNum);
   color(document.querySelector(`.box${opp}`), opp, currentColor);
 };
 
 const colorFill = boxNum => {
-  let clusterColor = gridBoxes.get(boxNum);
+  let clusterColor = getBoxColor(boxNum);
   if(clusterColor == currentColor) return;
   fillHelper(boxNum, clusterColor);
 };
@@ -124,21 +253,21 @@ const colorFill = boxNum => {
 //recursively fill the board to improve efficiency
 const fillHelper = (boxNum, clusterColor) => {
     //end the recursive call if it is not the same as the color of original box that was clicked
-    if(gridBoxes.get(boxNum) != clusterColor) return;
-    if(gridBoxes.get(boxNum) == clusterColor) color(document.querySelector(`.box${boxNum}`), boxNum, currentColor);
+    if(getBoxColor(boxNum) != clusterColor) return;
+    if(getBoxColor(boxNum) == clusterColor) color(getBoxElement(boxNum), boxNum, currentColor);
     
     gridBoxes.set(boxNum, currentColor);
     //recursively call the fillHelper for all boxes directly adjacent that are in bounds of the grid
     if(!(boxNum % gridSize == 1)) fillHelper(boxNum - 1, clusterColor);
     if(!(boxNum % gridSize == 0)) fillHelper(boxNum + 1, clusterColor);
-    if(!(boxNum - gridSize < 0)) fillHelper(boxNum - gridSize, clusterColor);
+    if(!(boxNum - gridSize <= 0)) fillHelper(boxNum - gridSize, clusterColor);
     if(!(+boxNum + +gridSize > gridSize * gridSize)) fillHelper(+boxNum + +gridSize, clusterColor);
 };
 
 //the function that is called that has calls to all drawing tools
 const updateBox = (boxNum, isClick) => {
     if(!mouseDown && !isClick) return;
-    const box = getBoxElemenet(boxNum);
+    const box = getBoxElement(boxNum);
     switch(currentTool) {
         case colorSelector: {
             //click only tool (no hover capabilities)
@@ -147,6 +276,7 @@ const updateBox = (boxNum, isClick) => {
         }
         case pen: {
             color(box, boxNum, currentColor);
+            isCleared = false;
             break;
         }
         case eraser: {
@@ -155,19 +285,23 @@ const updateBox = (boxNum, isClick) => {
         }
         case lighten: {
             lightenColor(box, boxNum);
+            isCleared = false;
             break;
         }
         case darken: {
             darkenColor(box, boxNum);
+            isCleared = false;
             break;
         }
         case mirrorDraw: {
             mirrorDrawing(box, boxNum);
+            isCleared = false;
             break;
         }
         case fill: {
             //click only tool (no hover capabilities)
             if(isClick) colorFill(boxNum);
+            isCleared = false;
             break;
         }
         default: {
@@ -176,19 +310,29 @@ const updateBox = (boxNum, isClick) => {
     }
 };
 
-//add variable hover highlight to give visual feedback to user
+
+grid.onmouseleave = () => document.body.style.cursor = "default";
+//add hover highlight to give visual feedback to user when using drawing tools
 const hoverEffect = boxNum => {
     if(currentTool == null) return;
-    const box = getBoxElemenet(boxNum);
+    const box = getBoxElement(boxNum);
     if(!mouseDown)
-        box.style.cursor = "pointer";
+        document.body.style.cursor = "pointer";
     else
-        box.style.cursor = "default";
+        document.body.style.cursor = "default";
     box.classList.toggle("hover");
     box.style.setProperty('--box-highlight', doubleVLight);
+
+    //gets rid of side borders if the box is on the edge of the grid
+    if(boxNum % gridSize == 1) box.classList.toggle("left-edge");
+    if(boxNum % gridSize == 0) box.classList.toggle("right-edge");
+    if(boxNum - gridSize <= 0) box.classList.toggle("top-edge");
+    if(+boxNum + +gridSize > gridSize * gridSize) box.classList.toggle("bottom-edge");
 };
 
 const clearBoard = () => {
+    if(isCleared) return;
+    isCleared = true;
     for(const boxNum of gridBoxes.keys()) {
         erase(document.querySelector(`.box${boxNum}`), boxNum);
     }
@@ -196,7 +340,11 @@ const clearBoard = () => {
 
 clear.addEventListener('click', clearBoard);
 
-const rotateLeft = () => {
+const rotateLeft = isHistory => {
+    if(!isHistory) {
+        history.updateHistory(new gridStatus());
+        history.current.addStatus(new changeData(null, null, "rotateLeft"));
+    }
     if(Math.abs(rotationDeg) == 270)
         rotationDeg = 0;
     else
@@ -204,7 +352,11 @@ const rotateLeft = () => {
     grid.style.transform = `rotate(${rotationDeg}deg)`;
 };
 
-const rotateRight = () => {
+const rotateRight = isHistory => {
+    if(!isHistory) {
+        history.updateHistory(new gridStatus());
+        history.current.addStatus(new changeData(null, null, "rotateRight"));
+    }
     if(Math.abs(rotationDeg) == 270)
         rotationDeg = 0;
     else
@@ -225,10 +377,10 @@ const flipColRow = (rcNum, rc) => {
             curr = ((i - 1) * gridSize) + rcNum;
             opp = calcOppVer(curr);
         }
-        const currBox = getBoxElemenet(curr);
-        const currColor = gridBoxes.get(curr);
-        const oppBox = getBoxElemenet(opp);
-        const oppColor = gridBoxes.get(opp);
+        const currBox = getBoxElement(curr);
+        const currColor = getBoxColor(curr);
+        const oppBox = getBoxElement(opp);
+        const oppColor = getBoxColor(opp);
         
         color(currBox, curr, oppColor);
         color(oppBox, opp, currColor)
@@ -236,14 +388,22 @@ const flipColRow = (rcNum, rc) => {
 };
 
 //flips every column
-const flipVer = () => {
+const flipVer = isHistory => {
+    if(!isHistory) {
+        history.updateHistory(new gridStatus());
+        history.current.addStatus(new changeData(null, null, "flipVer"));
+    }
     for(let i = 1; i <= gridSize; i++) {
         flipColRow(i, "col");
     }
 };
 
 //flips every row
-const flipHor = () => {
+const flipHor = isHistory => {
+    if(!isHistory) {
+        history.updateHistory(new gridStatus());
+        history.current.addStatus(new changeData(null, null, "flipHor"));
+    }
     for(let i = 0; i < gridSize; i++) {
         flipColRow(i, "row");
     }
@@ -253,29 +413,70 @@ const flipHor = () => {
 const flip = flipType => {
     if(flipType == 'hor') {
         if((Math.abs(rotationDeg)/90) % 2 == 0)
-            flipHor();
+            flipHor(false);
         else
-            flipVer();
+            flipVer(false);
     }
     else if(flipType == 'ver') {
         if((Math.abs(rotationDeg)/90) % 2 == 0)
-            flipVer();
+            flipVer(false);
         else
-            flipHor();
+            flipHor(false);
+    }
+}
+
+const undoRedo = (historyShift, doType) => {
+    if(historyShift.gridStatus.has("transform")) {
+        const transformation = historyShift.gridStatus.get("transform").transformation;
+
+        switch(transformation) {
+            case "rotateRight": {
+                if(doType == "undo") 
+                    rotateLeft(true);
+                else if(doType =="redo") 
+                    rotateRight(true);
+                break;
+            }
+            case "rotateLeft": {
+                if(doType == "undo") 
+                    rotateRight(true);
+                else if(doType =="redo") 
+                    rotateLeft(true);
+                break;
+            }
+            case "flipVer": {
+                flipVer(true);
+                break;
+            }
+            case "flipHor": {
+                flipHor(true);
+                break;
+            }
+            default: {
+                break;
+            }
+        }
     }
 }
 
 const undo = () => {
-
+    historyShift = history.undo();
+    console.log(historyShift);
+    console.log(history);
+    if(historyShift) undoRedo(historyShift, "undo");
 };
 
 const redo = () => {
-
+    historyShift = history.redo();
+    console.log(historyShift);
+    console.log(history);
+    if(historyShift) undoRedo(historyShift, "redo");
 };
 
 const addEvents = (box, boxNum) => {
     box.addEventListener("mouseover", updateBox.bind(this, boxNum, false));
-    box.addEventListener("mousedown", updateBox.bind(this, boxNum, true));  
+    box.addEventListener("mousedown", updateBox.bind(this, boxNum, true));
+    //hovereffect is applied when house goes into a box then is unapplied after it leaves the box
     box.addEventListener("mouseenter", hoverEffect.bind(this, boxNum));
     box.addEventListener("mouseleave", hoverEffect.bind(this, boxNum));
 }
@@ -349,8 +550,8 @@ drawingTools.forEach(tool => {
     tool.addEventListener('click', selectTool.bind(this, tool));
 });
 
-leftRotater.addEventListener('click', rotateLeft);
-rightRotater.addEventListener('click', rotateRight);
+leftRotater.addEventListener('click', rotateLeft.bind(this, false));
+rightRotater.addEventListener('click', rotateRight.bind(this, false));
 verticalFlipper.addEventListener('click', flip.bind(this, 'ver'));
 horizontalFlipper.addEventListener('click', flip.bind(this, 'hor'));
 undoButton.addEventListener('click', undo);
